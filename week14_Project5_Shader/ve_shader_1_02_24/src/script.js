5375b290e1f1ed70d41015c70f83c72faf2afe7c
cxxooo <cxxooo1203@gmail.com>
2024-12-13 12:46:45 -0800

    update

--- week14_Project5_Shader/ve_shader_1_02_24/src/script.js
+++ week14_Project5_Shader/ve_shader_1_02_24/src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from '../node_modules/three';
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js'
-import GUI from '../node_modules/lil-gui';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js'
+import GUI from './node_modules/lil-gui';
 import testVertexShader from './shaders/water/vertex.glsl';
 import testFragmentShader from './shaders/water/fragment.glsl';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
